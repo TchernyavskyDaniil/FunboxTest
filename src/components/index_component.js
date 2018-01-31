beaacadcdc973d54ba27ba6109d4926135e744b3
Ge11ert <gellert94@mail.ru>
2018-01-31 07:24:01 +0300

    Добавляет текст ответов

--- src/components/index_component.js
+++ src/components/index_component.js
@@ -4,12 +4,12 @@ import {Link} from 'react-router-dom';
 export default class IndexComponent extends React.Component {
   render () {
     return (
-      <section>
-        <h1>Выполненное тестовое задание для FunBox</h1>
-        <p>Для перехода к Level 1 или Level 2 нажмите соответствующую кнопку:</p>
-        <div>
-          <Link to="/level1">Level 1</Link>
-          <Link to="/level2">Level 2</Link>
+      <section className="start-page">
+        <h1 className="start-page__header">Выполненное тестовое задание для FunBox</h1>
+        <p className="start-page__info">Для перехода к Level 1 или Level 2 нажмите соответствующую кнопку:</p>
+        <div className="start-page__links-container">
+          <Link to="/level1" className="start-page__link">Level I</Link>
+          <Link to="/level2" className="start-page__link">Level II</Link>
         </div>
       </section>
     );
