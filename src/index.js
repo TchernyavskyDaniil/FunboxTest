4c58b416de0acaef7bfcce64e9d35db07e473972
Ge11ert <gellert94@mail.ru>
2018-01-31 21:45:44 +0300

    Тестовое изменение путей #2

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
       <Switch>
-        <Route path="/level1" component={Level1Component}/>
-        <Route path="/level2" component={Level2Component}/>
+        <Route path="/funboxtest/level1" component={Level1Component}/>
+        <Route path="/funboxtest/level2" component={Level2Component}/>
         <Route path="/" component={IndexComponent}/>
       </Switch>
     </BrowserRouter>
