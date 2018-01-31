f48032bb475b9923afa1497987925babf83f05d0
Ge11ert <gellert94@mail.ru>
2018-01-31 21:42:38 +0300

    Тестовое изменение путей

--- src/components/level2_component.js
+++ src/components/level2_component.js
@@ -8,7 +8,7 @@ export default class Level2Component extends React.Component {
     return (
       <section className="level-two">
         <h1 className="level-two__title">Level 2</h1>
-        <Link to="/" className="level-two__back link-button link-button--lev-2">Назад на главную</Link>
+        <Link to="/funboxtest" className="level-two__back link-button link-button--lev-2">Назад на главную</Link>
         <article className="level-two__map routes">
           <h2 className="routes__title">Редактор маршрутов</h2>
           <PointsEditor/>
