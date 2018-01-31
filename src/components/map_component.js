26d748eb7a6bc3d7a1b5c24d0010f7f88f921c0d
Ge11ert <gellert94@mail.ru>
2018-01-31 17:59:15 +0300

    Добавляет отображение линий между точками на карте

--- src/components/map_component.js
+++ src/components/map_component.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {YMaps, Map, Placemark} from 'react-yandex-maps';
+import {YMaps, Map, Placemark, Polyline} from 'react-yandex-maps';
 
 const mapState = {center: [55.755, 37.622], zoom: 12}; //55.755412, 37.621595
 
 class RoutesMap extends React.Component {
-  addPlacemark () {
+  constructor () {
+    super();
+    this.state = {
+      placemarks: {}
+    };
+  }
+
+  renderPlacemarks () {
     const points = this.props.pointsList;
 
-    return Object.values(points).map((point) => {
+    const placemarks = Object.values(points).map((point) => {
       return (
         <Placemark key={point.id}
           geometry={{type: 'Point', coordinates: mapState.center}}
           options={{preset: 'islands#blueCircleDotIcon', draggable: true}}
           properties={{hintContent: point.title}}
+          onGeometryChange={(event) => this.onGeometryChange(event, point.id)}
         />
       );
     });
+
+    return placemarks;
+  }
+
+  onGeometryChange ({originalEvent}, id) {
+    const placemarks = this.state.placemarks;
+
+    placemarks[id] = originalEvent.target.geometry.getCoordinates();
+    this.setState({placemarks});
+  }
+
+  renderLine () {
+    if (Object.keys(this.state.placemarks).length > 1) {
+      return (
+        <Polyline
+          geometry={{coordinates: Object.values(this.state.placemarks)}}
+          options={{ strokeColor: '#000000', strokeWidth: 1, strokeOpacity: 0.7}}
+        />
+      );
+    }
+
+    return '';
   }
 
   render () {
@@ -24,7 +54,8 @@ class RoutesMap extends React.Component {
       <YMaps>
         <div>
           <Map state={mapState}>
-            {this.addPlacemark()}
+            {this.renderPlacemarks()}
+            {this.renderLine()}
           </Map>
         </div>
       </YMaps>
